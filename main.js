1ea270016f1959e86de0fb2d6ce9aa5884a1a2eb
KellyLWilson <60358839+KellyLWilson@users.noreply.github.com>
2020-03-01 17:42:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -69,14 +69,13 @@ function populateRandom() {
 
 }
 
-//function viewStr() {  
-  //  if (view == 5) {       
-    //    document.getElementById("str").style.visibility = "visible";        
-   // } else {
-     //   document.getElementById("str").style.visibility = "hidden";       
- //   }
-
-// }
+function viewStr() {  
+    if (view == 5) {       
+        document.getElementById("str").style.visibility = "visible";        
+    } else {
+        document.getElementById("str").style.visibility = "hidden";       
+    }
+}
 
 
 function hideButton1() {
