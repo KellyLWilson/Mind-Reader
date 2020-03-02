6517f1b10fedcd4a5d32413b3147d65925b2586d
KellyLWilson <60358839+KellyLWilson@users.noreply.github.com>
2020-03-01 19:46:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ function getRandom() {
             random = "<span>" + i + ":" + charArray[Math.floor(Math.random() * charArray.length)] + "</span><br />";
         }
         randomStrings.push(random);
-        let singleStr = str;
+        
         
         
     }
-    //additional.push(str);
+    let singleStr = str;    //additional.push(str);
 }
 
 
